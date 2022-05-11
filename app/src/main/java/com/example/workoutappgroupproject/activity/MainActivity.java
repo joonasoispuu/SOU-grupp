d033b51d75233a0a1a06e14f8e4adde939fb27ea
Jan-MarcusSivadi <jmarcus@hot.ee>
2022-05-11 08:07:38 +0300

    Enchance current Fragments, Database and layouts

--- app/src/main/java/com/example/workoutappgroupproject/activity/MainActivity.java
+++ app/src/main/java/com/example/workoutappgroupproject/activity/MainActivity.java
@@ -1,42 +1,30 @@
 package com.example.workoutappgroupproject.activity;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatEditText;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.text.InputType;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.example.workoutappgroupproject.ExampleDialog;
 import com.example.workoutappgroupproject.R;
 import com.example.workoutappgroupproject.databinding.ActivityMainBinding;
+import com.example.workoutappgroupproject.fragment.ArmsandchestFragment;
+import com.example.workoutappgroupproject.fragment.CustomFragment;
 import com.example.workoutappgroupproject.fragment.ProfileFragment;
 import com.example.workoutappgroupproject.fragment.RunFragment;
+import com.example.workoutappgroupproject.fragment.SixpackFragment;
 import com.example.workoutappgroupproject.fragment.TrainFragment;
 import com.example.workoutappgroupproject.room.User;
 import com.example.workoutappgroupproject.room.UserViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -48,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout mainView;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor sharedEditor;
+//    Button btnSaveProfile;
+    int oldId;
+    private Boolean isExists = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,90 +49,66 @@ public class MainActivity extends AppCompatActivity {
         mainView = findViewById(R.id.mainView);
         // init view model
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        userViewModel.getIsExists().observe(this, isExists -> { this.isExists = isExists; });
 
         // check if app is on it's first run
         sharedPreferences = getPreferences(Context.MODE_PRIVATE);
         sharedEditor = sharedPreferences.edit();
-        if (isItFirestTime()) {
+        if (isItFirstTime()) {
             System.out.println("First time");
             // TODO: prompt user to enter data
         } else {
             System.out.println("Not First Time");
             // get users data
-            userViewModel.getAllUsers().observe(this, this::getUserData);
+//            userViewModel.getAllUsers().observe(this, this::getUserData);
         }
 
-        /*
-        userViewModel.getIsExists().observe(this, isExists ->{
-            if (!isExists) {
-                // no users in db
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Welcome!");
-                builder.setMessage("What's your name?");
-                builder.setCancelable(false);
-                View view = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null);
-                final EditText input = (EditText) view.findViewById(R.id.etName);
-                builder.setView(view);
-                // builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-                builder.setPositiveButton("Save", (dialog, which) -> {});
-                AlertDialog alert = builder.create();
-                alert.show();
-                alert.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String first = input.getText().toString().trim();
-                        String last = input.getText().toString().trim();
-                        float height = 0;
-                        float weight = 0;
-                        int age = 19;
-                        if (TextUtils.isEmpty(first) || TextUtils.isEmpty(last)) {
-                            input.setError("Field can't be empty!");
-                            return;
-                        }
-                        // save data to db ...
-                        User user = new User(first,last,height,weight,age);
-                        userViewModel.insert(user);
-                        Toast.makeText(MainActivity.this, "Name saved to db " + input.getText().toString(), Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
-            } else {
-                // users exist in db
-                userViewModel.getAllUsers().observe(this,users -> {
-                    int pos = 0;
-                    while (pos < users.size()){
-                        getUserData(users,pos);
-                        pos++;
-                    }
-                });
-            }
-        });
-         */
+
+
         // TODO: ask user for name, height, weight
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         // set navigation bar position to profileFragment by default
         bottomNavigationView.setSelectedItemId(R.id.profileFragment);
-        replaceFragment(new ProfileFragment());
+        //replaceFragment(new ProfileFragment(),1,false);
+
         // bottom nav logic
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             // never select an already selected item from bottom navbar
-            if(item.getItemId() == bottomNavigationView.getSelectedItemId()) return false;
+            if (item.getItemId() == bottomNavigationView.getSelectedItemId()) return false;
             switch(item.getItemId()){
                 case R.id.runFragment:
-                    replaceFragment(new RunFragment());
+                    // check if user exists
+                    if (!isExists) {
+                        openDialog();
+                        return false;
+                    }
+                    replaceFragment(new RunFragment(),-1,false);
                     break;
                 case R.id.profileFragment:
-                    replaceFragment(new ProfileFragment());
+                    if(oldId != R.id.runFragment) replaceFragment(new ProfileFragment(),-1,false);
+                    else replaceFragment(new ProfileFragment(),1,false);
                     break;
                 case R.id.trainFragment:
-                    replaceFragment(new TrainFragment());
+                    // check if user exists
+                    if (!isExists) {
+                        openDialog();
+                        return false;
+                    }
+                    replaceFragment(new TrainFragment(),1,false);
                     break;
             }
+            oldId = item.getItemId();
             return true;
         });
+
     }
 
-    private boolean isItFirestTime() {
+    private void openDialog() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(),"example dialog");
+    }
+
+    private boolean isItFirstTime() {
         if (sharedPreferences.getBoolean("firstTime", true)) {
             sharedEditor.putBoolean("firstTime", false);
             sharedEditor.commit();
@@ -152,28 +119,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getUserData(List<User> users) {
-        int pos = 0;
-        // iterate users list
-        while (pos < users.size()){
-            String first = users.get(pos).getFirstName();
-            String last = users.get(pos).getLastName();
-            float height = users.get(pos).getHeight();
-            float weight = users.get(pos).getWeight();
-            int age = users.get(pos).getAge();
-            System.out.println("---USER---"+"\n"+ "id: "+pos+"\n"+ "first: "+first+
-                    "\n"+ "last: "+last+"\n"+ "height: "+height+"\n"+
-                    "weight: "+weight+ "\n"+ "age: "+age+"\n");
-            pos++;
+    private void replaceFragment(Fragment fragment, int dir, boolean backStack){
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (dir >= 1) fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        else fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+        if (backStack) fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    //On TrainFragment user can choose what exercise do to by having the fragment replaced
+    public void OnChosenExercise(View view){
+        String Exercise = view.getTag().toString();
+        switch (Exercise){
+            case "ArmsandChest":
+                replaceFragment(new ArmsandchestFragment(),1,true);
+                break;
+            case "Sixpack":
+                replaceFragment(new SixpackFragment(),1,true);
+                break;
+            case "Custom":
+                replaceFragment(new CustomFragment(),1,true);
+                break;
         }
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager =getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
-        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+    private boolean hasUserData(List<User> users) {
+        int pos = 0;
+        // iterate users list
+        while (pos < users.size()){
+            String name = users.get(pos).getName();
+            float height = users.get(pos).getHeight();
+            float weight = users.get(pos).getWeight();
+            int age = users.get(pos).getAge();
+            System.out.println("---USER---"+"\n"+ "id: "+pos+"\n"+ "name: "+name +
+                    "\n"+"height: "+height+"\n"+ "weight: "+weight+ "\n"+ "age: "+age+"\n");
+            pos++;
+        }
+        return pos > 0;
+    }
+
+    private LiveData<Boolean> isExists() {
+        return userViewModel.getIsExists();
     }
 }
--- app/src/main/java/com/example/workoutappgroupproject/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/workoutappgroupproject/fragment/ProfileFragment.java
@@ -5,23 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.workoutappgroupproject.R;
+import com.example.workoutappgroupproject.room.User;
+import com.example.workoutappgroupproject.room.UserViewModel;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
-    public static final String EXTRA_FIRST = "com.nagel.lab5.activity.EXTRA_TITLE";
-    public static final String EXTRA_LAST = "com.nagel.lab5.activity.EXTRA_AUTHOR";
-    public static final String EXTRA_CONTENT = "com.nagel.lab5.activity.EXTRA_CONTENT";
-    public static final String EXTRA_TIME = "com.nagel.lab5.activity.EXTRA_TIME";
-    public static final String EXTRA_ID = "com.nagel.lab5.activity.EXTRA_ID";
+    private UserViewModel userViewModel;
 
     public static final int RESULT_SAVE = 100;
     public static final int RESULT_EDIT = 200;
+    private String dataStatus = "Empty";
+
+    TextInputLayout textInputName, textInputHeight, textInputWeight, textInputAge;
+    Button btnSaveProfile, btnRemoveProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -29,14 +38,14 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("state",dataStatus);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -44,5 +53,210 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textInputName = view.findViewById(R.id.textInputName);
+        textInputHeight = view.findViewById(R.id.textInputHeight);
+        textInputWeight = view.findViewById(R.id.textInputWeight);
+        textInputAge = view.findViewById(R.id.textInputAge);
+        btnSaveProfile = view.findViewById(R.id.btnSaveProfile);
+        btnRemoveProfile = view.findViewById(R.id.btnRemoveProfile);
+        TextView title = view.findViewById(R.id.Profile);
+        title.setText(dataStatus);
+
+        // init view model
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+        // check if data already there
+        userViewModel.getIsExists().observe(getViewLifecycleOwner(), isExists ->{
+            if (!isExists) {
+//                // no users in db
+//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setTitle("Welcome!");
+//                builder.setMessage("What's your name?");
+//                builder.setCancelable(false);
+//                View view = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null);
+//                final EditText input = (EditText) view.findViewById(R.id.etName);
+//                builder.setView(view);
+//                // builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+//                builder.setPositiveButton("Save", (dialog, which) -> {});
+//                AlertDialog alert = builder.create();
+//                alert.show();
+//                alert.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        String first = input.getText().toString().trim();
+//                        String last = input.getText().toString().trim();
+//                        float height = 0;
+//                        float weight = 0;
+//                        int age = 19;
+//                        if (TextUtils.isEmpty(first) || TextUtils.isEmpty(last)) {
+//                            input.setError("Field can't be empty!");
+//                            return;
+//                        }
+//                        // save data to db ...
+//                        User user = new User(first,last,height,weight,age);
+//                        userViewModel.insert(user);
+//                        Toast.makeText(getActivity(), "Name saved to db " + input.getText().toString(), Toast.LENGTH_LONG)
+//                                .show();
+//                    }
+//                });
+            } else {
+                // users exist in db
+                userViewModel.getAllUsers().observe(getViewLifecycleOwner(),users -> {
+                    boolean gotUsers = hasUserData(users);
+                    if (gotUsers) {
+                        int pos = 0;
+                        // iterate users list
+                        while (pos < users.size()){
+                            String name = users.get(pos).getName();
+                            float height = users.get(pos).getHeight();
+                            float weight = users.get(pos).getWeight();
+                            int age = users.get(pos).getAge();
+                            pos++;
+                            textInputName.getEditText().setText(name);
+                            textInputHeight.getEditText().setText(String.valueOf(height));
+                            textInputWeight.getEditText().setText(String.valueOf(weight));
+                            textInputAge.getEditText().setText(String.valueOf(age));
+                        }
+                        textInputName.setCounterEnabled(false);
+                        textInputName.setEnabled(false);
+                        textInputHeight.setEnabled(false);
+                        textInputWeight.setEnabled(false);
+                        textInputAge.setEnabled(false);
+                        btnSaveProfile.setText(R.string.btn_edit);
+                        if (savedInstanceState == null) {
+                            dataStatus = "Saved";
+                        }
+                        else if (dataStatus.equals("Empty")) {
+                            dataStatus = savedInstanceState.getString("state","Empty");
+                        }
+                    }
+                    // reload user form when data loads (on screen rotation)
+                    if(dataStatus.equals("Editing")){
+                        textInputName.setCounterEnabled(true);
+                        textInputName.setEnabled(true);
+                        textInputHeight.setEnabled(true);
+                        textInputWeight.setEnabled(true);
+                        textInputAge.setEnabled(false);
+                        btnSaveProfile.setText(R.string.btn_update);
+                        btnRemoveProfile.setEnabled(false);
+                    }
+                    title.setText(dataStatus);
+                });
+            }
+        });
+        // save button click listener
+        btnSaveProfile.setOnClickListener(view1 -> {
+            if (dataStatus.equals("Empty")){
+                if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
+                    // cancel
+                    return;
+                }
+                String name = textInputName.getEditText().getText().toString();
+                float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
+                float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
+                int age = Integer.parseInt(textInputAge.getEditText().getText().toString());
+                // save data to db ...
+                User user = new User(name,height,weight,age);
+                userViewModel.insert(user);
+                //Toast.makeText(getActivity(), "Data saved: " + name, Toast.LENGTH_LONG)
+                //        .show();
+                dataStatus = "Saved";
+            } else if (dataStatus.equals("Saved")) {
+                textInputName.setCounterEnabled(true);
+                textInputName.setEnabled(true);
+                textInputHeight.setEnabled(true);
+                textInputWeight.setEnabled(true);
+                textInputAge.setEnabled(false);
+                btnSaveProfile.setText(R.string.btn_update);
+                btnRemoveProfile.setEnabled(false);
+                dataStatus = "Editing";
+            } else if (dataStatus.equals("Editing")) {
+                if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
+                    // cancel
+                    return;
+                }
+                String name = textInputName.getEditText().getText().toString();
+                float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
+                float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
+                int age = Integer.parseInt(textInputAge.getEditText().getText().toString());
+                // save updated data to db ...
+                User user = new User(name,height,weight,age);
+                user.setId(1);
+                userViewModel.update(user);
+                //Toast.makeText(getActivity(), "Data updated: " + name, Toast.LENGTH_LONG)
+                //        .show();
+                btnRemoveProfile.setEnabled(true);
+                dataStatus = "Saved";
+            } else {
+            }
+            title.setText(dataStatus);
+        });
+    }
+
+    private boolean validateWeight(View v) {
+        String weightInput = textInputWeight.getEditText().getText().toString().trim();
+        if (weightInput.isEmpty()) {
+            textInputWeight.setError("Field cannot be empty!");
+            return false;
+        } else {
+            textInputWeight.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateHeight(View v) {
+        String heightInput = textInputHeight.getEditText().getText().toString().trim();
+        if (heightInput.isEmpty()) {
+            textInputHeight.setError("Field cannot be empty!");
+            return false;
+        } else {
+            textInputHeight.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateName(View v) {
+        String nameInput = textInputName.getEditText().getText().toString().trim();
+        if (nameInput.isEmpty()) {
+            textInputName.setError("Field cannot be empty!");
+            return false;
+        } else if (nameInput.length() > 20) {
+            textInputName.setError("Field cannot contain more than 20 characters!");
+            return false;
+        } else {
+            textInputName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateAge(View v) {
+        String ageInput = textInputAge.getEditText().getText().toString().trim();
+        if (ageInput.isEmpty()) {
+            textInputAge.setError("Field cannot be empty!");
+            return false;
+        } else {
+            textInputAge.setError(null);
+            return true;
+        }
+    }
+
+    private boolean hasUserData(List<User> users) {
+        int pos = 0;
+        // iterate users list
+        while (pos < users.size()){
+            String name = users.get(pos).getName();
+            float height = users.get(pos).getHeight();
+            float weight = users.get(pos).getWeight();
+            int age = users.get(pos).getAge();
+            System.out.println("---USER---"+"\n"+ "id: "+pos+"\n"+ "name: "+name +
+                    "\n"+"height: "+height+"\n"+ "weight: "+weight+ "\n"+ "age: "+age+"\n");
+            pos++;
+        }
+        return pos > 0;
     }
 }
