0244cd33d7b55540a413f2efdecc878568fb6508
Jan-MarcusSivadi <jmarcus@hot.ee>
2022-05-22 20:44:12 +0300

    Improve ProfileFragment field validation, Fix issue #25, Fix issue #10, Fix issue #34, Fix issue #35, Fix over-constrained layouts

--- app/src/main/java/com/example/workoutappgroupproject/activity/CustomActivity.java
+++ app/src/main/java/com/example/workoutappgroupproject/activity/CustomActivity.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.example.workoutappgroupproject.R;
--- app/src/main/java/com/example/workoutappgroupproject/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/workoutappgroupproject/fragment/ProfileFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +18,10 @@ import android.widget.Toast;
 import com.example.workoutappgroupproject.R;
 import com.example.workoutappgroupproject.room.User;
 import com.example.workoutappgroupproject.room.UserViewModel;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileFragment extends Fragment {
@@ -27,10 +30,11 @@ public class ProfileFragment extends Fragment {
 
     public static final int RESULT_SAVE = 100;
     public static final int RESULT_EDIT = 200;
+    private static final ArrayList<User> userArrayList = new ArrayList<>();
     private String dataStatus = "Empty";
 
     TextInputLayout textInputName, textInputHeight, textInputWeight, textInputAge;
-    Button btnSaveProfile, btnRemoveProfile;
+    Button btnSaveProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -41,6 +45,7 @@ public class ProfileFragment extends Fragment {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("state",dataStatus);
+        System.out.println("onSaveInstanceState dataStatus: "+dataStatus);
     }
 
     @Override
@@ -63,147 +68,154 @@ public class ProfileFragment extends Fragment {
         textInputWeight = view.findViewById(R.id.textInputWeight);
         textInputAge = view.findViewById(R.id.textInputAge);
         btnSaveProfile = view.findViewById(R.id.btnSaveProfile);
-        btnRemoveProfile = view.findViewById(R.id.btnRemoveProfile);
-        TextView title = view.findViewById(R.id.Profile);
-        title.setText(dataStatus);
 
         // init view model
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
+        // save dataStatus to savedInstanceState key value set
+        if (savedInstanceState != null) {
+            dataStatus = savedInstanceState.getString("state","Empty");
+        }
+
         // check if data already there
-        userViewModel.getIsExists().observe(getViewLifecycleOwner(), isExists ->{
+        userViewModel.getIsExists().observe(getViewLifecycleOwner(), isExists -> {
             if (!isExists) {
-//                // no users in db
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Welcome!");
-//                builder.setMessage("What's your name?");
-//                builder.setCancelable(false);
-//                View view = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null);
-//                final EditText input = (EditText) view.findViewById(R.id.etName);
-//                builder.setView(view);
-//                // builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-//                builder.setPositiveButton("Save", (dialog, which) -> {});
-//                AlertDialog alert = builder.create();
-//                alert.show();
-//                alert.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        String first = input.getText().toString().trim();
-//                        String last = input.getText().toString().trim();
-//                        float height = 0;
-//                        float weight = 0;
-//                        int age = 19;
-//                        if (TextUtils.isEmpty(first) || TextUtils.isEmpty(last)) {
-//                            input.setError("Field can't be empty!");
-//                            return;
-//                        }
-//                        // save data to db ...
-//                        User user = new User(first,last,height,weight,age);
-//                        userViewModel.insert(user);
-//                        Toast.makeText(getActivity(), "Name saved to db " + input.getText().toString(), Toast.LENGTH_LONG)
-//                                .show();
-//                    }
-//                });
+                // no users in db
+                System.out.println("current dataStatus: "+dataStatus);
             } else {
                 // users exist in db
                 userViewModel.getAllUsers().observe(getViewLifecycleOwner(),users -> {
                     boolean gotUsers = hasUserData(users);
                     if (gotUsers) {
-                        int pos = 0;
-                        // iterate users list
-                        while (pos < users.size()){
-                            String name = users.get(pos).getName();
-                            float height = users.get(pos).getHeight();
-                            float weight = users.get(pos).getWeight();
-                            int age = users.get(pos).getAge();
-                            pos++;
+                        if (!dataStatus.equals("Editing")) {
+                            int id = 0; // <-- first User object in list
+                            String name = users.get(id).getName();
                             textInputName.getEditText().setText(name);
+                            float height = users.get(id).getHeight();
                             textInputHeight.getEditText().setText(String.valueOf(height));
+                            float weight = users.get(id).getWeight();
                             textInputWeight.getEditText().setText(String.valueOf(weight));
+                            int age = users.get(id).getAge();
                             textInputAge.getEditText().setText(String.valueOf(age));
-                        }
-                        textInputName.setCounterEnabled(false);
-                        textInputName.setEnabled(false);
-                        textInputHeight.setEnabled(false);
-                        textInputWeight.setEnabled(false);
-                        textInputAge.setEnabled(false);
-                        btnSaveProfile.setText(R.string.btn_edit);
-                        if (savedInstanceState == null) {
+                            // set textInputs
+                            textInputName.setCounterEnabled(false);
+                            textInputName.setEnabled(false);
+                            textInputHeight.setEnabled(false);
+                            textInputWeight.setEnabled(false);
+                            textInputAge.setEnabled(false);
+                            btnSaveProfile.setText(R.string.btn_edit);
                             dataStatus = "Saved";
                         }
-                        else if (dataStatus.equals("Empty")) {
-                            dataStatus = savedInstanceState.getString("state","Empty");
-                        }
+                        System.out.println("current dataStatus: "+dataStatus);
                     }
-                    // reload user form when data loads (on screen rotation)
-                    if(dataStatus.equals("Editing")){
-                        textInputName.setCounterEnabled(true);
-                        textInputName.setEnabled(true);
-                        textInputHeight.setEnabled(true);
-                        textInputWeight.setEnabled(true);
-                        textInputAge.setEnabled(false);
-                        btnSaveProfile.setText(R.string.btn_update);
-                        btnRemoveProfile.setEnabled(false);
-                    }
-                    title.setText(dataStatus);
                 });
             }
-        });
-        // save button click listener
-        btnSaveProfile.setOnClickListener(view1 -> {
-            if (dataStatus.equals("Empty")){
-                if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
-                    // cancel
-                    return;
-                }
-                String name = textInputName.getEditText().getText().toString();
-                float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
-                float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
-                int age = Integer.parseInt(textInputAge.getEditText().getText().toString());
-                // save data to db ...
-                User user = new User(name,height,weight,age);
-                userViewModel.insert(user);
-                //Toast.makeText(getActivity(), "Data saved: " + name, Toast.LENGTH_LONG)
-                //        .show();
-                dataStatus = "Saved";
-            } else if (dataStatus.equals("Saved")) {
+            // when current status == Editing
+            if(dataStatus.equals("Editing")){
                 textInputName.setCounterEnabled(true);
                 textInputName.setEnabled(true);
                 textInputHeight.setEnabled(true);
                 textInputWeight.setEnabled(true);
-                textInputAge.setEnabled(false);
+                textInputAge.setEnabled(true);
                 btnSaveProfile.setText(R.string.btn_update);
-                btnRemoveProfile.setEnabled(false);
-                dataStatus = "Editing";
-            } else if (dataStatus.equals("Editing")) {
-                if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
-                    // cancel
-                    return;
-                }
-                String name = textInputName.getEditText().getText().toString();
-                float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
-                float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
-                int age = Integer.parseInt(textInputAge.getEditText().getText().toString());
-                // save updated data to db ...
-                User user = new User(name,height,weight,age);
-                user.setId(1);
-                userViewModel.update(user);
-                //Toast.makeText(getActivity(), "Data updated: " + name, Toast.LENGTH_LONG)
-                //        .show();
-                btnRemoveProfile.setEnabled(true);
-                dataStatus = "Saved";
-            } else {
             }
-            title.setText(dataStatus);
         });
+        // save button click listener
+        btnSaveProfile.setOnClickListener(view1 -> {
+            switch (dataStatus) {
+                case "Empty": {
+                    if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
+                        // cancel
+                        return;
+                    }
+                    String name = textInputName.getEditText().getText().toString();
+                    float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
+                    float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
+                    int age = 0;
+                    try {
+                        age = Integer.parseInt(textInputAge.getEditText().getText().toString());
+                    } catch (NumberFormatException nfe) {
+                        textInputAge.getEditText().setError("ERROR");
+                        System.out.println("NumberFormat Exception: invalid input string");
+                    }
+                    // save data to db ...
+                    User user = new User(name,height,weight,age);
+                    userViewModel.insert(user);
+                    Toast.makeText(getActivity(), "User created successfully! ", Toast.LENGTH_LONG)
+                            .show();
+                    break;
+                }
+                case "Saved": {
+                    textInputName.setCounterEnabled(true);
+                    textInputName.setEnabled(true);
+                    textInputHeight.setEnabled(true);
+                    textInputWeight.setEnabled(true);
+                    textInputAge.setEnabled(true);
+                    btnSaveProfile.setText(R.string.btn_update);
+                    dataStatus = "Editing";
+                    System.out.println("current dataStatus: "+dataStatus);
+                    break;
+                }
+                case "Editing": {
+                    if (!validateName(view) | !validateHeight(view) | !validateWeight(view) | !validateAge(view)){
+                        // cancel
+                        return;
+                    } else {
+                        if (!validateUpdate(0)) return;
+                    }
+                    String name = textInputName.getEditText().getText().toString();
+                    float height = Float.parseFloat(textInputHeight.getEditText().getText().toString());
+                    float weight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
+                    int age = Integer.parseInt(textInputAge.getEditText().getText().toString());
+                    // save updated data to db ...
+                    User user = new User(name,height,weight,age);
+                    user.setId(1);
+                    userViewModel.update(user);
+                    Toast.makeText(getActivity(), "User updated successfully! ", Toast.LENGTH_LONG)
+                            .show();
+                    dataStatus = "Saved";
+                    break;
+                }
+            }
+        });
+    }
+
+    private boolean validateUpdate(int id) {
+        List<User> users = userViewModel.getAllUsers().getValue();
+
+        String name = users.get(id).getName();
+        String oldName = textInputName.getEditText().getText().toString();
+
+        float height = users.get(id).getHeight();
+        float oldHeight = Float.parseFloat(textInputHeight.getEditText().getText().toString());
+
+        float weight = users.get(id).getWeight();
+        float oldWeight = Float.parseFloat(textInputWeight.getEditText().getText().toString());
+
+        int age = users.get(id).getAge();
+        int oldAge = Integer.parseInt(textInputAge.getEditText().getText().toString());
+
+        // if no field is different from current User data
+        if (name.equals(oldName) && height == oldHeight && weight == oldWeight && age == oldAge) {
+            Snackbar.make(getActivity().findViewById(R.id.mainView),
+                    "Can't save already existing data. Make sure at least 1 field is unique", 5000).show();
+            return false;
+        }
+        return true;
     }
 
     private boolean validateWeight(View v) {
         String weightInput = textInputWeight.getEditText().getText().toString().trim();
+
         if (weightInput.isEmpty()) {
             textInputWeight.setError("Field cannot be empty!");
             return false;
         } else {
+            float weight = Float.parseFloat(weightInput);
+            if (weight > 635) {
+                textInputWeight.setError("Too fat! Max: 635kg");
+                return false;
+            }
             textInputWeight.setError(null);
             return true;
         }
@@ -211,10 +223,16 @@ public class ProfileFragment extends Fragment {
 
     private boolean validateHeight(View v) {
         String heightInput = textInputHeight.getEditText().getText().toString().trim();
+
         if (heightInput.isEmpty()) {
             textInputHeight.setError("Field cannot be empty!");
             return false;
         } else {
+            float height = Float.parseFloat(heightInput);
+            if (height > 272) {
+                textInputHeight.setError("Too tall! Max: 272cm");
+                return false;
+            }
             textInputHeight.setError(null);
             return true;
         }
@@ -239,6 +257,9 @@ public class ProfileFragment extends Fragment {
         if (ageInput.isEmpty()) {
             textInputAge.setError("Field cannot be empty!");
             return false;
+        } else if (ageInput.length() > 3) {
+            textInputAge.setError("Too large number!");
+            return false;
         } else {
             textInputAge.setError(null);
             return true;
@@ -246,17 +267,28 @@ public class ProfileFragment extends Fragment {
     }
 
     private boolean hasUserData(List<User> users) {
-        int pos = 0;
         // iterate users list
-        while (pos < users.size()){
-            String name = users.get(pos).getName();
-            float height = users.get(pos).getHeight();
-            float weight = users.get(pos).getWeight();
-            int age = users.get(pos).getAge();
-            System.out.println("---USER---"+"\n"+ "id: "+pos+"\n"+ "name: "+name +
+        int id = -1;
+        for(int i = 0; i<users.size(); i++){
+            id = users.get(i).getId();
+            String name = users.get(i).getName();
+            float height = users.get(i).getHeight();
+            float weight = users.get(i).getWeight();
+            int age = users.get(i).getAge();
+            System.out.println("---USER---"+"\n"+ "id: "+id+"\n"+ "name: "+name +
                     "\n"+"height: "+height+"\n"+ "weight: "+weight+ "\n"+ "age: "+age+"\n");
-            pos++;
         }
-        return pos > 0;
+        return (id != -1);
+//        int pos = 0;
+//        while (pos < users.size()){
+//            String name = users.get(pos).getName();
+//            float height = users.get(pos).getHeight();
+//            float weight = users.get(pos).getWeight();
+//            int age = users.get(pos).getAge();
+//            System.out.println("---USER---"+"\n"+ "id: "+pos+"\n"+ "name: "+name +
+//                    "\n"+"height: "+height+"\n"+ "weight: "+weight+ "\n"+ "age: "+age+"\n");
+//            pos++;
+//        }
+//        return pos > 0;
     }
 }
